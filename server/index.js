450cf166778de1a85a23bc607669a1c3de6331bb
Derek Mason <3derekmason@gmail.com>
2021-11-21 19:01:14 -0700

    set up server and database connection

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const Router = require('./routes.js');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(Router);
+
+app.listen(1703, () => {
+  console.log('Connected to the Log...')
+});
