15cfedca04df687e4e30b5d05dfc4be73cf7e882
Derek Mason <3derekmason@gmail.com>
2021-11-23 06:50:09 -0700

    set up server to test connection. On page load it displays data from woodPile collection via find

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const db = require('../db')
+
 //const Router = require('./routes.js');
 
 const app = express();
@@ -7,6 +9,17 @@ const app = express();
 app.use(express.json());
 
 //app.use(Router);
+
+app.get('/', (err, res) => {
+  if (err) {console.log('U Idiot')}
+
+  const workout = db.collection('woodPile');
+
+  workout.find({}).toArray((e, data) => {
+    if (e) throw e;
+    res.json(data);
+  })
+})
 
 app.listen(1703, () => {
   console.log('Sharpening the axe...')
