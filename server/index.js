b0f2293ad809207e03d936ab1956ef9db36baca0
Derek Mason <3derekmason@gmail.com>
2021-11-23 15:00:25 -0700

    comments/formatting

--- server/index.js
+++ server/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 
 const db = require('../db')
-
 const logUsers = require('../db/userIndex');
 
-//const Router = require('./routes.js');
-
 const app = express();
-
 app.use(express.json());
 
-//app.use(Router);
-
+/*
+ * * * * * * * * REQUESTS TO logusers : users * * * * * * * *
+*/
 app.post('/users', (req, res) => {
   const query = 'INSERT INTO users (username, email, date_joined) VALUES ($1, $2, $3)';
   const username = req.body.username;
@@ -22,7 +19,20 @@ app.post('/users', (req, res) => {
     res.status(201).send(`User,${username} given an axe`);
   })
 })
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM users';
+  logUsers.query(query, (err, data) => {
+    if (err) console.log(err);
+    res.status(200).json(data.rows);
+  })
+})
+/*
+ * * * * * * * * REQUESTS TO woodshed : woodPile * * * * * * * *
+*/
 
+
+
+// * * * * * * * * Server Connection * * * * * * * *
 app.listen(1703, () => {
   console.log('Sharpening the axe...')
 });
