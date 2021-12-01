380d9a083eeb6403e7741df1b4460644fff7faae
Derek Mason <3derekmason@gmail.com>
2021-12-01 08:16:22 -0700

    connected App and index.jsx to server. index.html init as well, ran babel for a bundle

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const db = require('../db')
 const logUsers = require('../db/userIndex');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 /*
  * * * * * * * * REQUESTS TO logusers : users * * * * * * * *
