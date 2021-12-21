a3020d319b47068dee25d270566795e667c79cf3
Derek Mason <3derekmason@gmail.com>
2021-12-21 10:48:24 -0700

    set up express-graphql server

--- server/index.js
+++ server/index.js
@@ -1,119 +1,118 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const db = require('../db')
-const logUsers = require('../db/userIndex');
+const logUsers = require("../db/userIndex");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, "../client/public")));
 
 /*
  * * * * * * * * REQUESTS TO logusers : users * * * * * * * *
-*/
-app.post('/users', (req, res) => {
-  const query = 'INSERT INTO users (username, email, date_joined) VALUES ($1, $2, $3)';
+ */
+app.post("/users", (req, res) => {
+  const query =
+    "INSERT INTO users (username, email, date_joined) VALUES ($1, $2, $3)";
   const username = req.body.username;
   const email = req.body.email;
   const date_joined = new Date();
   logUsers.query(query, [username, email, date_joined], (err, results) => {
     if (err) console.log(err);
     res.status(201).send(`User,${username} given an axe`);
-  })
-})
-app.get('/users', (req, res) => {
-  const query = 'SELECT * FROM users';
+  });
+});
+app.get("/users", (req, res) => {
+  const query = "SELECT * FROM users";
   logUsers.query(query, (err, data) => {
     if (err) console.log(err);
     res.status(200).json(data.rows);
-  })
-})
+  });
+});
 /*
  * * * * * * * * REQUESTS TO logusers : core_library * * * * * * * *
-*/
-app.post('/library/core', (req, res) => {
-  const query = 'INSERT INTO core_library (exercise, equipment, added_by) VALUES ($1, $2, $3)';
+ */
+app.post("/library/core", (req, res) => {
+  const query =
+    "INSERT INTO core_library (exercise, equipment, added_by) VALUES ($1, $2, $3)";
   const exercise = req.body.exercise.toLowerCase();
   const equipment = req.body.equipment.toLowerCase();
   const addedBy = Number(req.body.added_by);
   logUsers.query(query, [exercise, equipment, addedBy], (err, results) => {
     if (err) console.log(err);
-    res.status(201).send(`Exercise, ${exercise} thown on the core_library pile...`);
-  })
-})
-app.get('/library/core', (req, res) => {
-  const query = 'SELECT * FROM core_library';
+    res
+      .status(201)
+      .send(`Exercise, ${exercise} thown on the core_library pile...`);
+  });
+});
+app.get("/library/core", (req, res) => {
+  const query = "SELECT * FROM core_library";
   logUsers.query(query, (err, data) => {
     if (err) console.log(err);
     res.status(200).json(data.rows);
-  })
-})
+  });
+});
 /*
  * * * * * * * * REQUESTS TO logusers : push_library * * * * * * * *
-*/
-app.post('/library/push', (req, res) => {
-  const query = 'INSERT INTO push_library (exercise, area, is_primary, added_by) VALUES ($1, $2, $3, $4)';
+ */
+app.post("/library/push", (req, res) => {
+  const query =
+    "INSERT INTO push_library (exercise, area, is_primary, added_by) VALUES ($1, $2, $3, $4)";
   const exercise = req.body.exercise.toLowerCase();
   const area = req.body.area.toLowerCase();
   const isPrimary = req.body.isPrimary;
-  const added_by = Number(req.body.added_by)
-  logUsers.query(query, [exercise, area, isPrimary, added_by], (err, results) => {
-    if (err) console.log(err);
-    res.status(201).send(`Exercise, ${exercise} thown on the push_library pile...`);
-  })
-})
-app.get('/library/push', (req, res) => {
-  const query = 'SELECT * FROM push_library';
+  const added_by = Number(req.body.added_by);
+  logUsers.query(
+    query,
+    [exercise, area, isPrimary, added_by],
+    (err, results) => {
+      if (err) console.log(err);
+      res
+        .status(201)
+        .send(`Exercise, ${exercise} thown on the push_library pile...`);
+    }
+  );
+});
+app.get("/library/push", (req, res) => {
+  const query = "SELECT * FROM push_library";
   logUsers.query(query, (err, data) => {
     if (err) console.log(err);
     res.status(200).json(data.rows);
-  })
-})
+  });
+});
 /*
  * * * * * * * * REQUESTS TO logusers : pull_library * * * * * * * *
-*/
-app.post('/library/pull', (req, res) => {
-  const query = 'INSERT INTO pull_library (exercise, area, is_primary, added_by) VALUES ($1, $2, $3, $4)';
+ */
+app.post("/library/pull", (req, res) => {
+  const query =
+    "INSERT INTO pull_library (exercise, area, is_primary, added_by) VALUES ($1, $2, $3, $4)";
   const exercise = req.body.exercise.toLowerCase();
   const area = req.body.area.toLowerCase();
   const isPrimary = req.body.isPrimary;
-  const added_by = Number(req.body.added_by)
-  logUsers.query(query, [exercise, area, isPrimary, added_by], (err, results) => {
-    if (err) console.log(err);
-    res.status(201).send(`Exercise, ${exercise} thown on the pull_library pile...`);
-  })
-})
-app.get('/library/pull', (req, res) => {
-  const query = 'SELECT * FROM pull_library';
+  const added_by = Number(req.body.added_by);
+  logUsers.query(
+    query,
+    [exercise, area, isPrimary, added_by],
+    (err, results) => {
+      if (err) console.log(err);
+      res
+        .status(201)
+        .send(`Exercise, ${exercise} thown on the pull_library pile...`);
+    }
+  );
+});
+app.get("/library/pull", (req, res) => {
+  const query = "SELECT * FROM pull_library";
   logUsers.query(query, (err, data) => {
     if (err) console.log(err);
     res.status(200).json(data.rows);
-  })
-})
+  });
+});
 
 /*
  * * * * * * * * REQUESTS TO woodshed : woodPile * * * * * * * *
-*/
-app.get('/woodpile', (req, res) => {
-  const database = db;
-  database.collection('woodPile').find({}).toArray((err, data) => {
-    if (err) throw err;
-    res.json(data);
-  })
-})
-
-// REQ QUERIES NEEDED : user_id, group
-app.post('/woodpile', (req, res) => {
-  const database = db;
-  const userID = Number(req.query.user_id);
-  const group = req.query.group;
-  const exercises = req.body;
-  db.collection('woodPile').insertOne({"user-id": userID, "group": group, "exercises": exercises});
-  res.status(201).send(`Workout for User ${userID} thrown on the log!`)
-})
-
+ */
 
 // * * * * * * * * Server Connection * * * * * * * *
 app.listen(1703, () => {
-  console.log('Sharpening the axe...')
+  console.log("Sharpening the axe...");
 });
