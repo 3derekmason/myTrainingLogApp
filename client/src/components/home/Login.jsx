21a46592b988606ec78e639eff84c3819bdbf21f
Derek Mason <3derekmason@gmail.com>
2022-01-05 16:07:05 -0700

    very basic login skeleton

--- client/src/components/home/Login.jsx
+++ client/src/components/home/Login.jsx
@@ -1,4 +1,4 @@
-import { Container, Typography } from "@material-ui/core";
+import { Button, Container, Typography, TextField } from "@material-ui/core";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navigate } from "react-router-dom";
@@ -7,8 +7,38 @@ const Login = () => {
   return (
     <div className="loginPage">
       <Container>
-        <div className="loginHead"></div>
-        <div className="loginForm"></div>
+        <div className="loginHead">
+          <Typography component="h1" variant="h3">
+            My Training Log App
+          </Typography>
+          <Typography component="h3" variant="h5">
+            Log in to your log...
+          </Typography>
+        </div>
+        <div className="loginForm">
+          <form>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username"
+              defaultValue=""
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password  *"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+            />
+            <Typography component="h6" variant="caption">
+              * required field
+            </Typography>
+            <Button variant="contained" color="primary">
+              Log In
+            </Button>
+          </form>
+        </div>
         <div className="loginFoot">
           <Typography component="h4" variant="caption">
             Not a user? Sign up{" "}
