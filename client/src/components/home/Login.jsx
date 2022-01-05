479e354f01874dfb06a94e20fbcf156331d92b6e
Derek Mason <3derekmason@gmail.com>
2022-01-05 15:57:37 -0700

    added link to signup from login/home

--- client/src/components/home/Login.jsx
+++ client/src/components/home/Login.jsx
@@ -1,9 +1,24 @@
+import { Container, Typography } from "@material-ui/core";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <div>Login Page</div>
+    <div className="loginPage">
+      <Container>
+        <div className="loginHead"></div>
+        <div className="loginForm"></div>
+        <div className="loginFoot">
+          <Typography component="h4" variant="caption">
+            Not a user? Sign up{" "}
+            <Link to="/signup" variant="body2">
+              here
+            </Link>
+            !
+          </Typography>
+        </div>
+      </Container>
     </div>
   );
 };
