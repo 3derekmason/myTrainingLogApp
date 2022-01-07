1b2d55aadfb73c882a965c54348ee3d38c517d4f
Derek Mason <3derekmason@gmail.com>
2022-01-07 13:57:01 -0700

    date formatting for landing page

--- client/src/components/home/Landing.jsx
+++ client/src/components/home/Landing.jsx
@@ -1,27 +1,96 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
+import { Navigate } from "react-router-dom";
+import { AppBar, Button, Card, Toolbar, Typography } from "@material-ui/core";
 import AppContext from "../context.js";
 
+const getMonth = (month) => {
+  const months = {
+    Jan: "January",
+    Feb: "February",
+    Mar: "March",
+    Apr: "April",
+    May: "May",
+    Jun: "June",
+    Jul: "July",
+    Aug: "August",
+    Sep: "September",
+    Oct: "October",
+    Nov: "November",
+    Dec: "December",
+  };
+  return months[month];
+};
+
+const getDayOfWeek = (day) => {
+  const days = {
+    Mon: "Monday",
+    Tue: "Tuesday",
+    Wed: "Wednesday",
+    Thu: "Thursday",
+    Fri: "Friday",
+    Sat: "Saturday",
+    Sun: "Sunday",
+  };
+
+  return days[day];
+};
+
+const makeDatePretty = (splitDate) => {
+  const dayOfWeek = getDayOfWeek(splitDate[0]);
+  const month = getMonth(splitDate[1]);
+  const day = splitDate[2];
+  const year = splitDate[3];
+
+  return `${dayOfWeek}, ${day} ${month} ${year}`;
+};
+
 const Landing = () => {
-  const { currentUser } = React.useContext(AppContext);
+  const { currentUser, setCurrentUser } = React.useContext(AppContext);
+  const [currentDate, setCurrentDate] = useState(new Date().toString());
+  const [prettyDate, setPrettyDate] = useState(
+    makeDatePretty(currentDate.split(" "))
+  );
+
+  useEffect(() => {
+    if (!currentDate) {
+      console.log("nope");
+    }
+
+    console.log(prettyDate);
+  }, [currentUser, currentDate]);
+
+  if (!currentUser) {
+    return <Navigate to="/" />;
+  }
+
   return (
-    <div>
-      <div>TRAINING LOG APP</div>
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <Button size="large">Log In</Button>
-      </Link>
-      <Link to="/signup" style={{ textDecoration: "none" }}>
-        <Button size="large">Sign Up</Button>
-      </Link>
-      <Button
-        onClick={(e) => {
-          e.preventDefault();
-          console.log(currentUser);
-        }}
-      >
-        Test
-      </Button>
+    <div className="landingPage">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography element="h5" variant="h5">
+            [] MTLA
+          </Typography>
+          <Button
+            onClick={(e) => {
+              e.preventDefault();
+              setCurrentUser(null);
+            }}
+          >
+            LOGOUT
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Card className="landingContainer">
+        <div className="landingHead">
+          <Typography element="h3" variant="h5">
+            Welcome, {currentUser.username}!
+          </Typography>
+          <Typography element="h5" variant="caption"></Typography>
+        </div>
+        <div className="landingPaths"></div>
+        <div className="landingFoot"></div>
+      </Card>
     </div>
   );
 };
