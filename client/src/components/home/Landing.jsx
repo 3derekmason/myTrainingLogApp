1d7b35ca6c19b90ce2c56c2932e67bbe15b7ad06
Derek Mason <3derekmason@gmail.com>
2022-01-07 14:30:16 -0700

    refactor to use intl.datetime

--- client/src/components/home/Landing.jsx
+++ client/src/components/home/Landing.jsx
@@ -4,52 +4,13 @@ import { Navigate } from "react-router-dom";
 import { AppBar, Button, Card, Toolbar, Typography } from "@material-ui/core";
 import AppContext from "../context.js";
 
-const getMonth = (month) => {
-  const months = {
-    Jan: "January",
-    Feb: "February",
-    Mar: "March",
-    Apr: "April",
-    May: "May",
-    Jun: "June",
-    Jul: "July",
-    Aug: "August",
-    Sep: "September",
-    Oct: "October",
-    Nov: "November",
-    Dec: "December",
-  };
-  return months[month];
-};
-
-const getDayOfWeek = (day) => {
-  const days = {
-    Mon: "Monday",
-    Tue: "Tuesday",
-    Wed: "Wednesday",
-    Thu: "Thursday",
-    Fri: "Friday",
-    Sat: "Saturday",
-    Sun: "Sunday",
-  };
-
-  return days[day];
-};
-
-const makeDatePretty = (splitDate) => {
-  const dayOfWeek = getDayOfWeek(splitDate[0]);
-  const month = getMonth(splitDate[1]);
-  const day = splitDate[2];
-  const year = splitDate[3];
-
-  return `${dayOfWeek}, ${day} ${month} ${year}`;
-};
-
 const Landing = () => {
   const { currentUser, setCurrentUser } = React.useContext(AppContext);
-  const [currentDate, setCurrentDate] = useState(new Date().toString());
-  const [prettyDate, setPrettyDate] = useState(
-    makeDatePretty(currentDate.split(" "))
+  const [currentDate, setCurrentDate] = useState(
+    new Intl.DateTimeFormat("en-GB", {
+      dateStyle: "full",
+      timeStyle: "long",
+    }).format(new Date())
   );
 
   useEffect(() => {
@@ -57,7 +18,8 @@ const Landing = () => {
       console.log("nope");
     }
 
-    console.log(prettyDate);
+    setCurrentDate(currentDate.split(" at ")[0]);
+    console.log(currentDate);
   }, [currentUser, currentDate]);
 
   if (!currentUser) {
@@ -86,7 +48,9 @@ const Landing = () => {
           <Typography element="h3" variant="h5">
             Welcome, {currentUser.username}!
           </Typography>
-          <Typography element="h5" variant="caption"></Typography>
+          <Typography element="h5" variant="caption">
+            {currentDate}
+          </Typography>
         </div>
         <div className="landingPaths"></div>
         <div className="landingFoot"></div>
