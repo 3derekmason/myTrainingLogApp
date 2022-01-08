6ea883f0c295ca0b28da23f3f8211bcac31a4c79
Derek Mason <3derekmason@gmail.com>
2022-01-08 06:20:07 -0700

    displayed desired data via triple map

--- client/src/components/user/UserLog.jsx
+++ client/src/components/user/UserLog.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import { AppBar, Button, Card, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  Card,
+  Container,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import AppContext from "../context.js";
 
 const UserLog = () => {
@@ -14,7 +21,11 @@ const UserLog = () => {
   if (!currentUser || currentUser.message) {
     return <Navigate to="/" />;
   }
-
+  if (!userWorkouts) {
+    return (
+      <div>Something went wrong, or you have not logged any workouts!</div>
+    );
+  }
   return (
     <div className="userLogPage">
       <AppBar position="static">
@@ -32,6 +43,33 @@ const UserLog = () => {
           </Button>
         </Toolbar>
       </AppBar>
+      <Container maxWidth="sm">
+        <div className="userLogTopBar"></div>
+        {userWorkouts?.map((workout, i) => {
+          return (
+            <Card key={i}>
+              {workout?.date}
+              {workout?.type}
+              {Object.keys(workout?.exercises).map((exercise, i) => {
+                return (
+                  <div className="logExercise" key={i}>
+                    {exercise}
+                    <ul>
+                      {workout.exercises[exercise].sets.map((set, i) => {
+                        return (
+                          <li key={i}>
+                            {set[0]} reps at {set[1]}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
+            </Card>
+          );
+        })}
+      </Container>
     </div>
   );
 };
