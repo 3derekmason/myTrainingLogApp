b2068da624e9fec4ffc574f5263171dc7f9752b1
Derek Mason <3derekmason@gmail.com>
2022-01-09 19:52:56 -0700

    add addWorkout route to express

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,8 @@ const controller = require("./controllers/index.js");
 router.post("/users/signup", controller.user.addNewUser);
 router.post("/users/login", controller.user.loginUser);
 
+router.post("/workouts", controller.workout.addWorkout);
+
 router.get("/workouts", controller.workout.getUserWorkouts);
 
 module.exports = router;
