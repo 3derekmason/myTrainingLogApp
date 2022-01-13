ef972ac2b7fad6d6ac13c76ee3e5afc89fcb846b
Derek Mason <3derekmason@gmail.com>
2022-01-13 11:46:02 -0700

    set up lastWorkout card skeleton with collapse button

--- client/src/components/home/LastWorkout.jsx
+++ client/src/components/home/LastWorkout.jsx
@@ -1,11 +1,32 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import { Button, Card, Paper, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AppContext from "../context.js";
 
 const LastWorkout = () => {
-  const { currentUser, lastWorkout } = React.useContext(AppContext);
+  const { currentUser, lastWorkout } = useContext(AppContext);
+  const [expanded, setExpanded] = useState(false);
+
+  const expandStyle = {
+    transform: "rotate(180deg)",
+  };
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   if (!currentUser || currentUser.message) {
     return <Navigate to="/" />;
@@ -13,14 +34,36 @@ const LastWorkout = () => {
 
   return (
     <div className="workoutSummary">
-      <Paper>
-        Most recent Workout:
-        <Typography element="h6" variant="caption">
-          {JSON.stringify(lastWorkout[0]?.date)}
-          {JSON.stringify(lastWorkout[0]?.type)}
-          {JSON.stringify(lastWorkout[0]?.exercises)}
-        </Typography>
-      </Paper>
+      <Card className="lastWorkoutCard">
+        <CardHeader
+          title="Summary of Last Workout:"
+          subheader={lastWorkout[0]?.date}
+        />
+
+        <CardContent>
+          <Typography variant="body2" component="p">
+            Most recent workout summary
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon style={expanded ? expandStyle : {}} />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Workout:</Typography>
+            <Typography paragraph>Row of exercise</Typography>
+            <Typography paragraph>another Exercise Row</Typography>
+            <Typography paragraph>another exercise row</Typography>
+            <Typography>another exercise row?</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     </div>
   );
 };
