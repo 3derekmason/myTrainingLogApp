24d43da10948b7107d862b5846192b1ae74c901c
Derek Mason <3derekmason@gmail.com>
2022-01-16 15:26:27 -0700

    set primay theme colors

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import App from "./components/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#5d4037",
+    },
+    secondary: {
+      main: "#bf360c",
+    },
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("app")
+);
